4df5277a66f86894d223f23bae135b8c97ebe5f2
Haritsst <muhammadharitsetiawan50@gmail.com>
2025-05-31 22:50:55 +0700

    Update NaiveBayes_HAR.py

--- NaiveBayes_HAR.py
+++ NaiveBayes_HAR.py
@@ -21,9 +21,7 @@ st.set_page_config(
 
 def load_data():
     df = pd.read_csv("heart_attack_prediction_dataset.csv")
-    cols_to_drop = ['Patient ID', 'Exercise Hours Per Week', 'Previous Heart Problems', 'Medication Use',
-                    'Stress Level', 'Sedentary Hours Per Day', 'Income', 'BMI',
-                    'Physical Activity Days Per Week', 'Country', 'Continent', 'Hemisphere']
+    cols_to_drop = ['Patient ID', 'Country', 'Continent', 'Hemisphere']
     df.drop(columns=[col for col in cols_to_drop if col in df.columns], inplace=True)
     df = pd.get_dummies(df, columns=['Sex', 'Diet'], drop_first=True)
     df[['Systolic_BP', 'Diastolic_BP']] = df['Blood Pressure'].str.split('/', expand=True).astype(int)
